167964025ef5b43f2d8f44bf179b7eeb0111fc5a
emmanuel sam <fndm4@gmail.com>
2025-03-24 14:26:33 +0100

    fixed api url issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,9 @@
-import Cards from "./Cards";
-import products from "../data/data";
-import Navbar from "./Navbar";
 import PageLayout from "../../layout/PageLayout";
 
 const Home = () => {
   return (
     <>
-    <PageLayout />
-      {/* <Navbar />
-
-      <div className="relative top-20">
-        <Cards products={products} />
-      </div> */}
+      <PageLayout />
     </>
   );
 };
